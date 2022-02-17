f4f6c62c93c0bd6745ba215267102e0fc64554d1
ericmtodd <eterictodd@gmail.com>
2022-02-16 19:15:36 -0700

    Nav now slides.

--- app/javascript/packs/components/NavBar.js
+++ app/javascript/packs/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = (props) => {
   const {
@@ -7,14 +8,18 @@ const NavBar = (props) => {
   } = props
 
   const toggleNav = () => {
-    console.log('toggleNav')
     let navIcon = document.querySelector('#nav-icon')
+    let navLinks = document.querySelector('#nav-links')
     if (navIcon.classList.contains('inactive-nav-icon')) {
       navIcon.classList.remove('inactive-nav-icon')
       navIcon.classList.add('active-nav-icon')
+      navLinks.classList.remove('hidden-nav-links')
+      navLinks.classList.add('shown-nav-links')
     } else {
       navIcon.classList.remove('active-nav-icon')
       navIcon.classList.add('inactive-nav-icon')
+      navLinks.classList.remove('shown-nav-links')
+      navLinks.classList.add('hidden-nav-links')
     }
   }
 
@@ -33,6 +38,9 @@ const NavBar = (props) => {
         <span />
         <span />
         <span />
+      </div>
+      <div id='nav-links' className='hidden-nav-links'>
+        <Link to='/' onClick={() => toggleNav()}>About</Link>
       </div>
     </div>
   )
