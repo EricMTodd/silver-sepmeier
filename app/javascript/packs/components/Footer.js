b37977362558bb8b8c940b299d6795d659dbe58b
ericmtodd <eterictodd@gmail.com>
2022-02-20 01:35:33 -0700

    Minor styling changes.

--- app/javascript/packs/components/Footer.js
+++ app/javascript/packs/components/Footer.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Footer = () => {
   return(
     <div id='footer'>
-      Made with ❤️ by <a href='https://ericmtodd.herokuapp.com' target='_blank'>Eric M. Todd</a>
+      <div id='contact'>
+        email: silsepme@gmail.com | insta: @sigh.like.the.moon
+      </div>
+      <div id='dev-credit'>
+        Made with ❤️ by <a href='https://ericmtodd.herokuapp.com' target='_blank'>Eric M. Todd</a>
+      </div>
     </div>
   )
 }
