24c92a0872975f298a80d57149fb01f052a83183
ericmtodd <eterictodd@gmail.com>
2022-02-21 19:22:32 -0700

    Added images for contact links.

--- app/javascript/packs/components/Footer.js
+++ app/javascript/packs/components/Footer.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import instagramLogo from '../../../assets/images/instagram-logo.png'
+import emailIcon from '../../../assets/images/email-icon.png'
 
 const Footer = () => {
   return(
     <div id='footer'>
-      <div id='contact'>
-        email: silsepme@gmail.com | insta: @sigh.like.the.moon
-      </div>
-      <div id='dev-credit'>
+        {/* email: silsepme@gmail.com | insta: @sigh.like.the.moon */}
+        <a href='https://www.instagram.com/sigh.like.the.moon/' target='_blank'><img id='instagram-logo' src={instagramLogo} alt='instagram-logo' /></a>
+        <a href='mailto:silsepme@gmail.com'><img id='email-icon' src={emailIcon} alt='email-icon' /></a>
+      {/* <div id='dev-credit'>
         Made with ❤️ by <a href='https://ericmtodd.herokuapp.com' target='_blank'>Eric M. Todd</a>
-      </div>
+      </div> */}
     </div>
   )
 }
